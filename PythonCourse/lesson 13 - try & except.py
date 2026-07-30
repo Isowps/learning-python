fdbfa7f1f3c7be9bdcba9b68d06732122295a2aa
Isowps <hanasan.benjamin.ross@gmail.com>
2026-07-30 10:25:05 +0800

    Enhance input validation with new challenges
    Added additional mini challenges and teacher challenge for input validation using try-except blocks.

--- PythonCourse/lesson 13 - try & except.py
+++ PythonCourse/lesson 13 - try & except.py
@@ -27,3 +27,39 @@ try:
     print("sum of both:", digit1 + digit2)
 except:
     print("invalid number!") 
+
+#round 2 
+#mini challenge 1
+try:
+    age = int(input("enter age: "))
+    print("You are", age,"years old")
+except:
+    print("enter a valid age!")
+
+#mini challenge 2
+while True:
+    try:
+        ages = int(input("enter your age: "))
+        print("You are", ages,"years old")
+        break
+    except:
+        print("enter a valid age! ")
+
+#mini project
+while True:
+    try:
+        numbers = int(input("enter a number: "))
+        print("Doubled:", numbers * 2)
+        break
+    except:
+        print("Invalid! Try again.")
+
+#teacher challenge
+while True:
+    try:
+        digit_1 = int(input("enter first digit: "))
+        digit_2 = int(input("enter second digit: "))
+        print(digit_1, digit_2)
+        break
+    except:
+        print("enter a valid number! ")
